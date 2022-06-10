cbd1a367485ec395a59fd33b9b87da9a74d641c8
Guo Benshuai <benshuai.guo@konicaminolta.com>
2022-06-10 14:26:52 +0900

    C complete

--- c_郭本帥.c
+++ c_郭本帥.c
@@ -1,5 +1,27 @@
 
 int main(void){
-  printf("hello kaku!\n");
+    int i;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("%d:KonicaMinolta\n", i);
+            continue;
+        }
+        else if (i % 5 == 0)
+        {
+            printf("%d:Minolta\n", i);
+            continue;
+        }
+        else if (i % 3== 0)
+        {
+            printf("%d:Konica\n", i);
+            continue;
+        }
+        else
+        {
+            printf("%d:\n", i);
+        }
+    }
   return 0;
   }
